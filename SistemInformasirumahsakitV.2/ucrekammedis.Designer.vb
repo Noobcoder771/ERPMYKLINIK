093babff3506b4674d7a51a62c6ed5a5711f8622
fauzan <oyes@DESKTOP-AUJ3I0M>
2025-12-04 22:36:20 +0700

    aigo

--- SistemInformasirumahsakitV.2/ucrekammedis.Designer.vb
+++ SistemInformasirumahsakitV.2/ucrekammedis.Designer.vb
@@ -652,7 +652,7 @@ Partial Class ucrekammedis
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(347, 9)
+        Me.Label1.Location = New System.Drawing.Point(338, 5)
         Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(261, 30)
